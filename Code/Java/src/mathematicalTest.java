9b788ace51c5fb54fde7e13d2f920b10fbb0aaf6
何朝晖 <hezhaohui@wust.edu.cn>
2024-08-27 16:46:40 +0800

    Update

---
+++ Code/Java/src/mathematicalTest.java
@@ -0,0 +1,9 @@
+class mathematicalTest {
+    public static void main(String[] arguments){
+        System.out.println(Math.sin(2));
+        System.out.println(Math.cos(Math.PI / 2));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.log(Math.log(3)));
+
+    }
+}
