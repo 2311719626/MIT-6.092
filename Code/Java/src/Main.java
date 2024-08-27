401b832cc829d71ff205beb379bed321264d3336
何朝晖 <hezhaohui@wust.edu.cn>
2024-08-27 14:48:38 +0800

    Code Update

---
+++ Code/Java/src/Main.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String[] arguments){
+        double score = 3.5;
+        int otherScore = (int)score;
+        System.out.println(score);
+        System.out.println(otherScore);
+
+        String text = "hello" + "world";
+        System.out.println(text);
+        text = text + "number " + 5;
+        System.out.println(text);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MIT-6.092
 [MIT-6.092] Lecture &amp; Assignment &amp; Code
+
+Please click [Courses](https://ocw.mit.edu/courses/6-092-introduction-to-programming-in-java-january-iap-2010/pages/syllabus/) to see all.
